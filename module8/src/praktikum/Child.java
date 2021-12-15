7f098641b6bf0b88f00654af168a966c82150ab1
Muhammad Irfani <muhammadirfani340@gmail.com>
2021-12-15 09:04:10 +0700

    commit_1

---
+++ module8/src/praktikum/Child.java
@@ -0,0 +1,10 @@
+package praktikum;
+
+public class Child extends Parent {
+    int x = 10;
+
+    @Override
+    public void info() {
+        System.out.println("Ini class Child");
+    }
+}
---
+++ module8/src/praktikum/Parent.java
@@ -0,0 +1,9 @@
+package praktikum;
+
+public class Parent {
+    int x = 5;
+
+    public void info() {
+        System.out.println("Ini class Parent");
+    }
+}
